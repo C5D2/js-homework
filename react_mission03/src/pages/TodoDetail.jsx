7a3dbab76f84375b2452f4b6afcadb47bcdfad9e
C5D2 <yoakec5d2@naver.com>
2024-03-19 18:27:49 +0900

    필요 없는 부분 주석 처리

--- react_mission03/src/pages/TodoDetail.jsx
+++ react_mission03/src/pages/TodoDetail.jsx
@@ -1,18 +1,19 @@
-import { Link, useParams } from 'react-router-dom';
-import useAxios from '../hooks/useAxios';
-import PropTypes from 'prop-types';
-import { ReactCsspin } from 'react-csspin';
+import { Link, useParams } from "react-router-dom";
+import useAxios from "../hooks/useAxios";
+import PropTypes from "prop-types";
+import { ReactCsspin } from "react-csspin";
 
-TodoDetail.propTypes = {
-  todoDetail: PropTypes.shape({
-    _id: PropTypes.number,
-    title: PropTypes.string,
-    content: PropTypes.string,
-    done: PropTypes.bool,
-    createdAt: PropTypes.string,
-    updatedAt: PropTypes.string,
-  }),
-};
+//  일단 props로 따로 내려주는 게 없어서 타입 지정은 필요 없음, interface 기능도 딱히 없는 듯하다..
+// TodoDetail.propTypes = {
+//   todoDetail: PropTypes.shape({
+//     _id: PropTypes.number,
+//     title: PropTypes.string,
+//     content: PropTypes.string,
+//     done: PropTypes.bool,
+//     createdAt: PropTypes.string,
+//     updatedAt: PropTypes.string,
+//   }),
+// };
 
 function TodoDetail() {
   const params = useParams();
@@ -27,17 +28,17 @@ function TodoDetail() {
       <h2>할일 상세 보기</h2>
 
       {isLoading && <ReactCsspin spinStyle="cp-morph" color="bisque" />}
-      {error && <p style={{ color: 'blueviolet' }}>{error.message}</p>}
+      {error && <p style={{ color: "blueviolet" }}>{error.message}</p>}
       <div className="todo">
         <div>{todoDetail.title}</div>
         <div>{todoDetail.content}</div>
         <div>
-          상태 :{' '}
+          상태 :{" "}
           {todoDetail.done === undefined
-            ? ''
+            ? ""
             : todoDetail.done
-            ? '완료'
-            : '미완료'}
+            ? "완료"
+            : "미완료"}
         </div>
         <div>작성일 : {todoDetail.createdAt}</div>
         <div>수정일 : {todoDetail.updatedAt}</div>
